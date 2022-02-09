e2af806596e649fc65460fd553a25c2a7c1b9bc8
RafaelJose10 <98101422+RafaelJose10@users.noreply.github.com>
2022-02-09 14:35:57 -0300

    1

--- Jogo/misseis.js
+++ Jogo/misseis.js
@@ -2,7 +2,11 @@ var largura_tela, py_missil, y, missil_local_surgimento, quantidade_missil, tipo
 var todos_misseis = new Array();
 var missil_caindo = new Array();
 var missil_caindo2 = new Array();
+var missil_X = new Array();
+var missil_src = new Array();
 let quant;
+
+var score = 0;
 
 
 function inicia_misseis() {
@@ -29,6 +33,8 @@ function cria_missil() {
 
     quantidade_missil = Math.round(Math.random(0) * 4);
 
+    // missil_src
+
 
     if (missil_caindo.length <= quant) {
         let i = missil_caindo.length;
@@ -38,6 +44,8 @@ function cria_missil() {
         missil_caindo[i].style.left = missil_local_surgimento + 'px';
         window.document.body.appendChild(missil_caindo[i]);
         missil_caindo2[i] = py_missil;
+        missil_X[i] = missil_local_surgimento;
+        missil_src[i] = 'imagens/missil_' + tipo_missil + '.png';
 
 
         // missil_caindo.push(todos_misseis[tipo_missil]);
@@ -53,17 +61,68 @@ function cai_missil() {
     for (let c = 0; c <= missil_caindo.length; c++) {
         missil_caindo2[c] += 7;
         missil_caindo[c].style.top = missil_caindo2[c] + 'px';
-        if (missil_caindo2[c] >= 390) {
-            missil_caindo[c].remove();
-            missil_caindo.splice(c, 1);
-            missil_caindo2.splice(c, 1)
-            missil_caindo2[c] = -40;
-            // shift(missil_caindo[c])
-        }
-        if (missil_caindo2[c] >= 380 && missil_caindo2[c] <= 400) {
-            if ((missil_local_surgimento - 40) <= px && (missil_local_surgimento + 40) >= px) {
-                alert("Perdeu")
+        if (missil_caindo2[c] >= 300 - 30) {
+            // impacto();
+            // alert(missil_caindo[c].src);
+            if (missil_src[c] == "imagens/missil_0.png") {
+                if (missil_caindo2[c] >= 335 - 30) {
+                    impacto();
+                }
+                if (missil_caindo2[c] >= 335) {
+                    // alert("missil 0")
+                    renova();
+                }
             }
+            if (missil_src[c] == 'imagens/missil_1.png') {
+                if (missil_caindo2[c] >= 370 - 30) {
+                    impacto();
+                }
+                if (missil_caindo2[c] >= 370) {
+                    renova();
+                    // alert('missil 1');
+                }
+            };
+            if (missil_src[c] == 'imagens/missil_2.png') {
+                if (missil_caindo2[c] >= 385 - 30) {
+                    impacto();
+                }
+                if (missil_caindo2[c] >= 385) {
+                    renova();
+                    // alert('missil 2');
+                }
+            };
+            if (missil_src[c] == 'imagens/missil_3.png') {
+                if (missil_caindo2[c] >= 370 - 30) {
+                    impacto();
+                }
+                if (missil_caindo2[c] >= 370) {
+                    renova()
+                        // alert('missil 3');
+                }
+            };
+
+            function impacto() {
+                let m_x = missil_X[c];
+                if (missil_caindo2[c] >= 300 - 30 && missil_caindo2[c] <= 400) {
+                    if ((m_x - 40) <= px && (m_x + 20) >= px) {
+                        // alert("Perdeu")
+                        renova();
+                        scores(score = 0)
+                    }
+                }
+            }
+
+
+            function renova() {
+                missil_caindo[c].remove();
+                missil_caindo.splice(c, 1);
+                missil_caindo2.splice(c, 1);
+                missil_X.splice(c, 1)
+                missil_src.splice(c, 1);
+                // score++;
+                scores(++score)
+            }
+            // alert(m_x + "==" + (m_x + 100));
         }
     }
 }
