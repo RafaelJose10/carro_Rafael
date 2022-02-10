71dde752d25a9b28d3062e747033cd8d8dc7223e
RafaelJose10 <98101422+RafaelJose10@users.noreply.github.com>
2022-02-10 00:17:15 -0300

    2

--- Jogo/cenario.js
+++ Jogo/cenario.js
@@ -5,7 +5,22 @@ function btn_inicicia() {
     var btn_esquerdo = window.document.querySelector('div#btn_esquerdo');
     var btn_direito = window.document.querySelector('div#btn_direito');
 
-    btn_esquerdo.style.width = (largura_tela / 2 - 25) + "px";
-    btn_direito.style.width = (largura_tela / 2 - 25) + 'px';
-    btn_direito.style.left = (largura_tela / 2) + 'px';
+
+    if (!(navigator.userAgent.match(/Android/i))) {
+        // alert("mobile")
+    } else {
+        let lado = (largura_tela / 16)
+        btn_esquerdo.style.width = (largura_tela / 4 - 25) + "px";
+        btn_esquerdo.style.height = (largura_tela / 4 - 25) + "px";
+        btn_esquerdo.style.left = lado + 'px'
+            // btn_esquerdo.style.top = 'calc(50% - 10px)';
+
+        btn_direito.style.width = (largura_tela / 4 - 25) + 'px';
+        btn_direito.style.height = (largura_tela / 4 - 25) + 'px';
+        btn_direito.style.left = ((window.innerWidth) - (largura_tela / 4)) + 'px'
+            // btn_direito.style.top = 'calc(50% - 10px)';
+
+        // btn_direito.style.left = (largura_tela / 2) + 'px';
+        // alert('nao mobile');
+    }
 };
